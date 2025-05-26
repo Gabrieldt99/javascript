9b8c97d7778dc668d8bf887888a00a0caf72e0dc
Gabriel Domakoski Trevisan <gabrielct.1818@gmail.com>
2025-05-26 20:15:33 -0300

    exios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log (`A velocidade do seu carro é ${vel} Km por hora.`)
+if (vel >= 61){ //Condição simples
+    console.log ('Você Ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log ('Dirija usando sempre cinto de segurança!')

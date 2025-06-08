045a2d4df101b689891ef6ec4374c5f769edfa2d
Gabriel Domakoski Trevisan <gabrielct.1818@gmail.com>
2025-06-08 20:02:34 -0300

    at

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,1 +1,13 @@
-console.log('Tudo bem?')
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=100)
+
+/*
+var c = 1 
+while (c < 100) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
